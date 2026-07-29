5ce0ed46f1f01feca14acf483c6fb211bf0ad7e1
Leo <leonardo.garcia@gnlsoftwaresolutions.com>
2026-07-28 21:48:26 -0700

    Add cascading auth state
    Add missing dependency to program.cs

--- src/OnboardMe.Web/Program.cs
+++ src/OnboardMe.Web/Program.cs
@@ -19,6 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorization();
 
 var githubClientId = builder.Configuration[GitHubClientIdConfigKey];
